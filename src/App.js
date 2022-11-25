f5a21339c200b5814f6416a626fb6f9dc8f78d1d
nazwa28 <nazwanadhipa28@gmail.com>
2022-11-25 22:31:38 +0700

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Cards from "./components/card";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-      </Routes>
+      <Navbar />
+      <HomePage heading='E-Flight Ticket' message='lets order now, dont regret it'/>
+      <Cards />
+        <Routes>
+          <Route path="/" />
+         
+        </Routes>
     </BrowserRouter>
   );
 }
