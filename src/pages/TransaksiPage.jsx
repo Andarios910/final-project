6d351f0ddb611addc168327c9838f41020fc38fb
Andarios910 <andarioskarokaro92@gmail.com>
2022-11-28 14:44:54 +0700

    add react-icon

--- src/pages/TransaksiPage.jsx
+++ src/pages/TransaksiPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsCircle } from 'react-icons/bs'
 
 export default function TransaksiPage() {
     return (
@@ -16,6 +17,29 @@ export default function TransaksiPage() {
                 </div>
                 <div className='w-2/6 bg-white rounded-md mt-10'>
                     <h2 className='text-center text-xl border-b py-2'>Your Trip Summary</h2>
+                    <p className='px-5 py-3'>Outbound Flight</p>
+                    <div className='px-5 py-2 flex justify-between'>
+                        <p>Sat, 26 Nov</p>
+                        <p>Sat, 26 Nov</p>
+                    </div>
+                    <div className='flex justify-between items-center px-5'>
+                        <div className=''>
+                            <p>18.20</p>
+                            <p>JKT</p>
+                        </div>
+                        <div>
+                            <p className='text-center'>8h 50m</p>
+                            <div className='flex items-center'>
+                                <BsCircle/>
+                                <hr className='bg-blue-700 w-32 font-semibold'/>
+                                <BsCircle/>
+                            </div>  
+                        </div>
+                        <div className='text-right'>
+                            <p>18.20</p>
+                            <p>JKT</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
