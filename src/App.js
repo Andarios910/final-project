415cca94280c8648f107dcaf08cf6d9c3da00b36
nazwa28 <nazwanadhipa28@gmail.com>
2022-11-29 16:11:41 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import Cards from "./components/card";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <HomePage heading='Find Next Place To Visit' message='lets order now, dont regret it'/>
-      <Cards />
-      <Contact />
-      <Footer />
-        {/* <Routes>
-          <Route path="/" />
+        <Routes> 
+          <Route path="/" element={<HomePage />}/>
          
-        </Routes> */}
+        </Routes>
     </BrowserRouter>
   );
 }
