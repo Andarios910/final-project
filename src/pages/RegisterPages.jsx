262e54c33791f85d4bef484ce28b73559486e7e4
khaerul <anaminformatika21@gmail.com>
2022-11-30 20:51:09 +0700

    add icons

--- src/pages/RegisterPages.jsx
+++ src/pages/RegisterPages.jsx
@@ -1,127 +1,133 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
-    const navigate = useNavigate();
-    
-    const [formValues, setFormValues] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
-    const [formErrors, setFormErrors] = useState({})
+  const navigate = useNavigate();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({...formValues, [name] : value });
+  const [formValues, setFormValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+  };
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.name) {
+      errors.name = "Masukan nama anda!";
+    } else if (!regex.test(values.name)) {
+      errors.name = "This is not a valid email format!";
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setFormErrors(validate(formValues))
-       
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
     }
-    
-    const validate = (values) => {
-        const errors = {};
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if (!values.email) {
-            errors.email = "Email is required!";
-        } else if (!regex.test(values.email)) {
-            errors.email = "This is not a valid email format!";
-        }
-        if (!values.password) {
-            errors.password = "Password is required";
-        } else if (values.password.length < 4) {
-            errors.password = "Password must be more than 4 characters";
-        } else if (values.password.length > 10) {
-            errors.password = "Password cannot exceed more than 10 characters";
-        }
-        return errors;
+    if (!values.password) {
+      errors.password = "Periksa Password Kembali";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password cannot exceed more than 10 characters";
     }
+    return errors;
+  };
 
-    const token = JSON.parse(localStorage.getItem('token'))
+  const token = JSON.parse(localStorage.getItem("token"));
 
-    useEffect(() => {
-        if (token) {
-            navigate('/')
-        }
-    }, [token, navigate])
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token, navigate]);
 
-    return (
-        <section className="bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="/" className="flex items-center mb-6 text-2xl font-semibold dark:text-white">
-                    {/* <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/> */}
-                    {/* NotFlix    */}Binar Air 
-                </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Create new account
-                        </h1>
-                        <form onSubmit={handleSubmit} className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama Lengkap</label>
-                                <input 
-                                    type="name" 
-                                    name="name" 
-                                    id="name" 
-                                    value={formValues.name}
-                                    onChange={handleChange}
-                                    className="bg-gray-700 border border-gray-600 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
-                                    placeholder="nama lengkap" 
-                                    required=""
-                                />
-                                <p className='text-red-600'>{formErrors.name}</p>
-                            </div>
-                            <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                <input 
-                                    type="email" 
-                                    name="email" 
-                                    id="email" 
-                                    value={formValues.email}
-                                    onChange={handleChange}
-                                    className="bg-gray-700 border border-gray-600 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
-                                    placeholder="email address" 
-                                    required=""
-                                />
-                                <p className='text-red-600'>{formErrors.email}</p>
-                            </div>
-                            <div>
-                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input 
-                                    type="password" 
-                                    name="password" 
-                                    id="password" 
-                                    value={formValues.password}
-                                    onChange={handleChange}
-                                    placeholder="pasword" 
-                                    className="bg-gray-700 border border-gray-600 text-white sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
-                                    required=""
-                                />
-                                <p className='text-red-600'>{formErrors.password}</p>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required=""/>
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="/" className="text-sm font-medium text-emerald-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign Up</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already a member ? <span onClick={() => navigate('/login')} className="font-medium text-emerald-600 hover:underline">Sign in</span>
-                            </p>
-                        </form>
-                    </div>
+  return (
+    <div>
+      <div className="w-full h-screen">
+        <img
+          src="https://cdn-2.tstatic.net/travel/foto/bank/images/nam-air_20161126_152947.jpg"
+          alt="/"
+          className="hidden sm:block absolute w-full h-full object-cover"
+        />
+
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-3 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Register</h1>
+              <form
+                onSubmit={handleSubmit}
+                className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded-lg"
+                  type="name"
+                  name="name"
+                  id="name"
+                  value={formValues.name}
+                  onChange={handleChange}
+                  placeholder="nama lengkap"
+                  required=""
+                />
+                <p className="text-red-600">{formErrors.name}</p>
+
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded-lg"
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={formValues.email}
+                  onChange={handleChange}
+                  placeholder="email address"
+                  required=""
+                />
+                <p className="text-red-600">{formErrors.email}</p>
+
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded-lg"
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={formValues.password}
+                  onChange={handleChange}
+                  placeholder="password"
+                  required=""
+                />
+                <p className="text-red-600">{formErrors.password}</p>
+
+                <button className="bg-cyan-600 hover:bg-cyan-700 py-3 my-6 rounded-lg font-bold">
+                  Register
+                </button>
+                <div className="flex justify-between items-center text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
                 </div>
+                <p className="py-8">
+                  <span className="text-white">Sudah Punya Akun?</span>{" "}
+                  <span
+                    onClick={() => navigate("/login")}
+                    className="font-medium text-cyan-600 hover:underline">
+                    Login
+                  </span>
+                </p>
+              </form>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
