ac07cbeb180f1baf19fa73132f4779c9b1fced05
Andarios910 <andarioskarokaro92@gmail.com>
2022-11-30 21:15:05 +0700

    fix problem

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SearchBar() {
     return (
         <div className='bg-white w-full h-64'>
-            tampan dan berani 
+            tampan dan berani asdfasdf
         </div>
     )
 }
