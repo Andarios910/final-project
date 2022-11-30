6eb46b9d54fad5aaa2d1afebb4985138ac755899
Andarios910 <andarioskarokaro92@gmail.com>
2022-11-30 19:21:47 +0700

    add component notification

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import Notification from './Notification'
 
 const Navbar = () => {
 
@@ -8,6 +9,8 @@ const Navbar = () => {
     const [ color, setColor ] = useState('transparent')
     const [ textColor, setTextColor ] = useState('white')
     const [ isOpen, setIsOpen ] = useState(false)
+
+    const [notif, setNotif] = useState(false)
 
     const handleNav = () => {
         setNav(!nav)
@@ -53,11 +56,14 @@ const Navbar = () => {
                 </li>     
             </ul>
             <div className='flex'>
-                <button href="#" className='block px-4 py-3 lg:py-5 text-white  hover:text-violet-500'>
+                <button onClick={() => setNotif(!notif)} className='block px-4 py-3 lg:py-5 text-white hover:text-violet-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
                     </svg>
                 </button>
+                {
+                    notif ? <Notification /> : <div></div>
+                }
                 <button href="#" className='flex flex-col lg:flex-row px-4 py-3 lg:py-5 text-white  hover:text-violet-500'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,26 +4,24 @@ import Navbar from '../components/Navbar'
 import Cards from '../components/card'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Notification from '../components/Notification'
 
 const HomePage = ({heading, message}) => {
   return (
     <>
-    <Navbar />
-    
-    <div className='flex items-center justify-center h-screen mb-[5rem] bg-fixed bg-center bg-cover custom-image'>
-        {/* Overlay */}
-        <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#051036]/60 z-[2]'/>
-        <div className='p-5 text-white z-[2] mb-[-6rem]'>
-            <h2 className='text-5xl font-bold'>Find Next Place To Visit</h2>
-            <p className='py-5 text-xl'>lets order now, dont regret it</p>
-            <button className='px-8 py-2 border'>See More</button>
-        </div>
-
-        
-    </div>
-    <Cards />
-    <Contact />
-    <Footer />
+      <Navbar />
+      <div className='flex items-center justify-center h-screen mb-[5rem] bg-fixed bg-center bg-cover custom-image'>
+          {/* Overlay */}
+          <div className='absolute top-0 left-0 right-0 bottom-0 bg-[#051036]/60 z-[2]'/>
+          <div className='p-5 text-white z-[2] mb-[-6rem]'>
+              <h2 className='text-5xl font-bold'>Find Next Place To Visit</h2>
+              <p className='py-5 text-xl'>lets order now, dont regret it</p>
+              <button className='px-8 py-2 border'>See More</button>
+          </div>
+      </div>
+      <Cards />
+      <Contact />
+      <Footer />
     </>
     
   )
