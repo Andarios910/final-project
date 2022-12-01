ec9b71a8d23f574b7ca0e64d9e5b0df816caafb3
khaerul anam <anaminformatika21@gmail.com>
2022-12-01 22:55:44 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<SearchPage />} />
+        <Route path="SearchPage" element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,298 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { BsCircle } from "react-icons/bs";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <div className="py-4 mx-60 gap-x-8">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-0 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-4 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-0 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-4 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-0 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+
+      <div className="py-4 mx-60">
+        <Card body>
+          <div className="flex items-center justify-between  bg-white rounded-md p-6">
+            <div>
+              <div className="flex justify-between items-center px-5">
+                <img
+                  className=" w-10"
+                  src="https://ik.imagekit.io/tvlk/image/imageResource/2015/12/17/1450350497772-d343e9b6823d18054324b818b999c394.png?tr=q-75"
+                  alt="nam-air"
+                />
+                <h1 className="px-5 text-xl font-medium">ANAM AIR</h1>
+                <div className="">
+                  <p>18.30</p>
+                  <p>Jakarta</p>
+                </div>
+                <div className="text-center">
+                  <p>2h 30m</p>
+                  <div className="flex justify-between items-center">
+                    <BsCircle />
+                    <div className="w-28 border-t border-black"></div>
+                    <BsCircle />
+                  </div>
+                </div>
+                <div className="text-right">
+                  <p>20.30</p>
+                  <p>Jogja</p>
+                </div>
+              </div>
+            </div>
+            <div className="px-5">
+              <h1 className="text-xl font-medium">
+                <span className="text-orange-600">Rp 1.150.000/</span>org
+              </h1>
+              <button className="bg-cyan-600 hover:bg-cyan-700 text-white px-10 py-2 rounded-lg">
+                Pilih
+              </button>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
