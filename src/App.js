00e6ef4bbffad25dec78c6001ac0b04b0afd141b
Andarios910 <andarioskarokaro92@gmail.com>
2022-12-08 13:41:19 +0700

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import OrderListDetail from "./pages/ProfilePages/OrderListDetail";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<OrderListDetail />} />
       </Routes>
     </BrowserRouter>
   );
