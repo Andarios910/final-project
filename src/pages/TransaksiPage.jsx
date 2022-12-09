715be658ea6ca6dd5a41242a4e2a768e7a2015a5
Andarios910 <andarioskarokaro92@gmail.com>
2022-12-09 14:13:06 +0700

    add navbar to transaksi page

--- src/pages/TransaksiPage.jsx
+++ src/pages/TransaksiPage.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import RightCard from '../components/TransaksiComponent/RightCard'
 import LeftCard from '../components/TransaksiComponent/LeftCard'
+import NavbarProfile from '../components/NavbarProfile'
 
 export default function TransaksiPage() {
     return (
-        <div className='h-full bg-[#e9ebee] pb-5'>
-            <h1 className='font-semibold text-2xl text-center'>Your Booking</h1>
-            <p className='text-center text-gray-600'>Fill in your details and review your booking.</p>  
-            <div className='md:flex md:justify-between max-w-[1024px] mx-auto mt-5 pb-10 px-5 md:px-0'>
-                <LeftCard />
-                <RightCard />
+        <>  
+            <NavbarProfile />
+            <div className='h-full bg-[#e9ebee] pb-5 mt-24'>
+                <h1 className='font-semibold text-2xl text-center'>Your Booking</h1>
+                <p className='text-center text-gray-600'>Fill in your details and review your booking.</p>  
+                <div className='md:flex md:justify-between max-w-[1024px] mx-auto mt-5 pb-10 px-5 md:px-0'>
+                    <LeftCard />
+                    <RightCard />
+                </div>
+                <div className='w-40 text-center text-white bg-cyan-600 hover:bg-cyan-700 py-3 px-8 rounded-2xl mx-auto'>
+                        Continue
+                </div>
             </div>
-            <div className='w-40 text-center text-white bg-cyan-600 hover:bg-cyan-700 py-3 px-8 rounded-2xl mx-auto'>
-                    Continue
-            </div>
-        </div>
+        </>
     )
 }
