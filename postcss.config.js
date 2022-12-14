066f416e96945003f33b45912f6a7572edc36b9f
khaerul anam <anaminformatika21@gmail.com>
2022-12-14 20:28:39 +0700

    bug

--- postcss.config.js
+++ postcss.config.js
@@ -4,19 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 };
-
-const colors = require("tailwindcss/colors");
-
-module.exports = {
-  mode: "jit",
-  theme: {
-    extend: {
-      colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
-      },
-    },
-  },
-  variants: {},
-  plugins: [],
-};
--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SearchPage from "./pages/SearchPage";
+
+import LoginPages from "./pages/LoginPages";
+import RegisterPages from "./pages/RegisterPages";
+import HomePage from "./pages/HomePage";
+import ProfilePages from "./pages/ProfilePages/ProfilePages";
+import OrderList from "./pages/ProfilePages/OrderList";
+import OrderListDetail from "./pages/ProfilePages/OrderListDetail";
+import TransaksiPage from "./pages/TransaksiPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="SearchPage" element={<SearchPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPages />} />
+        <Route path="/register" element={<RegisterPages />} />
+        <Route path="/user/profile" element={<ProfilePages />} />
+        <Route path="/user/order-list" element={<OrderList />} />
+        <Route path="/user/order-list/detail" element={<OrderListDetail />} />
+        <Route path="transaksi" element={<TransaksiPage />} />
       </Routes>
     </BrowserRouter>
   );
