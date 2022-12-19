c79c5c9836365c66995e9f4de5c9a687228bf9c3
nazwa28 <nazwanadhipa28@gmail.com>
2022-12-19 21:53:14 +0700

    add checkoutpage and payment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Checkout from './pages/checkout'
 import COpage from "./pages/COPage";
+import Payment from "./pages/payment";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<COpage/>}/>
+        <Route path="/" element={<Checkout/>}/>
+        <Route path="checkout" element={<Checkout/>}/>
+        <Route path="payment" element={<Payment/>}/>
+        <Route path="copage" element={<COpage/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/COPage.jsx
+++ src/pages/COPage.jsx
@@ -1,17 +1,49 @@
 import React from 'react'
-import Checkout from "../components/checkout"
+import { MdQrCode2 } from 'react-icons/md'
 
-export default function TransaksiPage() {
+export default function component() {
     return (
-        <div className='h-full w-full bg-[#e9ebee] '>
-            {/* <h1 className='font-semibold text-2xl text-center'>Your Booking</h1>
-            <p className='text-center text-gray-600'>Fill in your details and review your booking.</p>   */}
-            <div className='md:flex md:justify-between max-w-[1024px] mx-auto mt-5 pb-10 px-5 md:px-0'>
-               <Checkout />
+            <div className='w-full h-full bg-[#bdbdbd]' >
+                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
+                    {/* <div className='flex px-5'>
+                        <button><AiOutlineArrowLeft /></button>
+                        <h1 className='text-xl font-bold mx-2'>Payment</h1>
+                    </div>
+                    <h1 className='px-12 mx-6'>Order ID: <span>YFE45RDE</span></h1> */}
+                     <img className='mt-5 ml-12' src='https://api.pegipegi.com/images/airlines/web/JT.png'/>
+
+                    <div className="p-8 mx-10">   
+                        <h1 className='text-lg font-bold'>LION AIR</h1>
+                        <h1 className=''>Order ID : <span>YFE45RDE</span></h1>
+                        <div className='mt-3 ml-1'>
+                            <p>Jakarta (CGK) - SURABAYA (SUB)</p>
+                            <p>Jum'at, 30 Des 2022</p>
+                        </div>
+                        <p className='text-xl font-bold mt-3 ml-1'>-------------------------------------------------------------</p>
+                        <div>
+                            <div className='flex text-sm font-semibold mt-4'>
+                                <h1>Penumpang</h1>
+                                <h1 className='ml-14'>No ID</h1>
+                            </div>
+                            <div className='flex  text-indigo-500 font-bold'>
+                                <p className=''>NAMA PENUMPANG</p>
+                                <p className='ml-14'>ID PENUMPANG</p>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className=' text-sm font-semibold mt-4'>Class/Tipe Penumpang</h1>
+                            <p className='text-indigo-500 font-bold'>ECONOMIC</p>
+                        </div>
+
+                        <div>
+                            <h1 className='text-lg font-bold mt-5 text-gray-500 text-center'>Pindai Kode ini di gerbang</h1>
+                            <div className='flex justify-center pb-5 items-center mt-4'>
+                                <MdQrCode2 className='w-32 h-32'/>
+                            </div>
+                            <p className='text-sm text-gray-600 text-center'> 11AUI118A57F</p>
+                        </div>                     
+                    </div>
+                </div>
             </div>
-            {/* <div className='w-40 text-center text-white bg-cyan-600 hover:bg-cyan-700 py-3 px-8 rounded-2xl mx-auto'>
-                    Continue
-            </div> */}
-        </div>
     )
 }
