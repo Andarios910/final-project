77a2d33cf4fff2e482bf7643b6d095674cacd21b
Andarios910 <andarioskarokaro92@gmail.com>
2022-12-21 13:02:39 +0700

    integrasi history and history detail

--- src/components/features/login/loginSlice.jsx
+++ src/components/features/login/loginSlice.jsx
@@ -22,6 +22,25 @@ export const handleLogin = createAsyncThunk(
     }
 )
 
+// export const handleUpdateUser = createAsyncThunk(
+//     'updateUser/handleUpdateUser',
+//     async(formValues) => {
+//         try {
+//             const req = await axios.post(`${request.baseUrl}/auth/login`, {
+//                 email: formValues.email,
+//                 password: formValues.password
+//             })
+//             localStorage.setItem('token', JSON.stringify(req.data.data.access_token))
+//             localStorage.setItem('user', JSON.stringify(req.data.data.data))
+//             // setTimeout(() => {
+//             //     window.location.reload(1)
+//             // }, 1500)
+//         }catch(error) {
+//             console.error(error);
+//         }
+//     }
+// )
+
 const initialState = {
     logInWithEmailAndPassword: [],
     signInWithGoogle: [],
