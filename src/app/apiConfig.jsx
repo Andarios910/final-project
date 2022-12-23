2713415726820a4783efe417e851b3542a4a98b6
Andarios910 <andarioskarokaro92@gmail.com>
2022-12-23 17:10:49 +0700

    change baseurl

--- src/app/apiConfig.jsx
+++ src/app/apiConfig.jsx
@@ -1,5 +1,6 @@
 const request = {
-    baseUrl: `https://eflightticketb2-staging.up.railway.app/api`,
+    // baseUrl: `https://eflightticketb2-staging.up.railway.app/api`,
+    baseUrl: `https://anam-air.herokuapp.com/api`
 }
 
 export default request
